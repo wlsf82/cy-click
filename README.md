65f84c54dc710363ed8713ab90f997f649687735
Walmyr <wlsf82@gmail.com>
2022-06-05 16:26:08 +0200

    Create cypress project

---
+++ README.md
@@ -0,0 +1,3 @@
+# cy-click
+
+Sample project to demonstrate different situations when clicking with Cypress.
---
+++ cypress/e2e/click.cy.js
@@ -0,0 +1,48 @@
+describe('click.cy.js', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  context('Only clicks on visible and/or enabled buttons', () => {
+    it('"Click me" button', () => {
+      cy.contains('button', 'Click me')
+        .should('be.visible')
+        .click()
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+
+    it('"Hey, click me too!" button', () => {
+      cy.contains('button', 'click me too')
+        .click() // on.cypress.io/interacting-with-elements
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+
+    it('"Try me" button', () => {
+      cy.contains('button', 'Try me')
+        .click()
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+
+  context('Forces click before button visibility', () => {
+    it('"Click me button" - force: true', () => {
+      cy.contains('button', 'Click me')
+        .click({ force: true })
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+
+  context('Forces click on disabled button', () => {
+    it('"Try me" - force: true', () => {
+      cy.contains('button', 'Try me')
+        .should('be.enabled')
+        .click({ force: true })
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+})
